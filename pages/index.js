d3fcbd5d71092b30b21fa733eadd28700a45d3d4
Vignesh <vignesh77024@gmail.com>
2023-01-19 12:11:47 +0530

    get viewport dimensions with useEffect

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Index.module.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import MyCanvas from '../components/MyCanvas'
 import ButtonPanel from '../components/ButtonPanel';
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  let windowSize = useWindowSize()
+
+  console.log(windowSize)
   const [ isHoldingSpace, setIsHoldingSpace ] = useState(false)
 
   const [isDragging, setIsDragging] = useState(false);
@@ -29,6 +32,38 @@ export default function Home() {
     }
   }
   
+  // Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
+
   return (
     <>
       <Head>
@@ -38,6 +73,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main 
+      style={{
+        width: windowSize.width,
+        height: windowSize.height
+      }
+      }
       className={styles.main}
       onKeyDown={handleSpaceKeyDown}
       onKeyUp={handleSpaceKeyUp}
